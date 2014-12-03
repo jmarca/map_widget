daf7a348c052e43d6ca66fbfa45058e12c72b6e4
James E. Marca <james@activimetrics.com>
2014-12-02 16:42:48 -0800

    added server bit to generate hpms totals per grid

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ var ppass = process.env.PSQL_PASS ;
 var phost = process.env.PSQL_HOST ;
 var pport = process.env.PSQL_PORT || 5432;
 
-var app = express()
+var config_okay = require('config_okay')
+var path = require('path')
+var rootdir = path.normalize(__dirname)
+var config_file = rootdir+'/test.config.json'
+
 
 var serveStatic = require('serve-static')
 
@@ -15,10 +19,69 @@ var serveStatic = require('serve-static')
 var spatial_routes = require('spatial_routes')
 var carb_areas = spatial_routes.carb_areas
 var detectors  = spatial_routes.detectors
+var hpms_routes = require('hpms_locator').hpms_routes
+var queue = require("queue-async")
 
-app.use(serveStatic('public'))
-app.use(serveStatic('build'))
-carb_areas({},app)
+var grid_records= require('calvad_areas').grid_records
+
+var queries = require('calvad_grid_merge_sqlquery')
+var hpms_data_route = queries.hpms_data_route
+var hpms_data_nodetectors_route = queries.hpms_data_nodetectors_route
 
 
-app.listen(3000)
+// for forEach on objects. also for flatten
+var _ = require('lodash')
+
+var all_hpms_handler = function(config,app){
+    var ij_regex = /(\d*)_(\d*)/;
+    app.get('/hpms/data/:yr.:format?'
+           ,function(req,res,next){
+                // build one task for each grid cell
+                var year = req.params.yr
+                var tasks=
+                    _.map(grid_records,function(membership,cell_id){
+                        var re_result = ij_regex.exec(cell_id)
+                        return {'cell_id':cell_id
+                               ,'cell_i':re_result[1]
+                               ,'cell_j':re_result[2]
+                               ,'year':year
+                               ,'options':config
+                               }
+                    });
+                var q = queue(4);
+                res.setTimeout(0)
+                tasks.forEach(function(t) {
+                    q.defer(queries.hpms_data_handler,t);
+                });
+                q.awaitAll(function(error, results) {
+                    console.log("all done!");
+                    // spit out results stitched together
+                    results = _.flatten(results)
+                    results = _.filter(results,function(r){
+                              return r.f_system=='totals'
+                              })
+                    res.json(_.flatten(results))
+                    return null
+                })
+                return null
+            })
+    return null
+}
+
+
+config_okay(config_file,function(err,c){
+
+    var config ={'postgres':c.postgres
+                ,'couchdb':c.couchdb}
+
+    var app = express()
+    app.use(serveStatic('public'))
+    app.use(serveStatic('build'))
+    carb_areas(config,app)
+    hpms_routes(config,app)
+    hpms_data_route(config,app)
+    hpms_data_nodetectors_route(config,app)
+    all_hpms_handler(config,app)
+    app.listen(3000)
+
+})
