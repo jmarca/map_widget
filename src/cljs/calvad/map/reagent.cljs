d30bc41e6747b0554bfc16dc52f9ca5cdd9c1378
James E. Marca <james@activimetrics.com>
2016-09-01 16:48:22 -0700

    initial commit, clojure is pushing out a skeleton
    Long row to hoe

---
+++ src/cljs/calvad/map/reagent.cljs
@@ -0,0 +1,5 @@
+(ns calvad.map.reagent
+  (:require [reagent.core :as r :refer [render]] ;; refer render to be used in the init function
+            [domina.core :as dom :refer [by-id]]
+            [clojure.string :as s :refer [trim blank?]]
+            [cljsjs.remarkable]))
